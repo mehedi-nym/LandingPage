5ebc92fc0a72e3e6134eba8be596852db1f09abd
Mehedi Hasan Nayem <mnym71@gmail.com>
2025-09-23 18:23:57 +0600

    Edited file from mac(office)

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.menu-toggle');
-  const nav = document.querySelector('.main-nav');
-
-  toggleBtn.addEventListener('click', () => {
-    nav.classList.toggle('active');
-  });
